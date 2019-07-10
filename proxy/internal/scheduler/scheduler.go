76ac24f91bfc08c007f36c2689f4a5e17c78433a
Brian Terry <brianter@amazon.com>
2019-07-10 10:42:16 -0400

    Added dependency injection

--- proxy/internal/scheduler/scheduler.go
+++ proxy/internal/scheduler/scheduler.go
@@ -7,26 +7,13 @@ import (
 	"time"
 )
 
-/*
-   * Schedule a re-invocation of the executing handler no less than 1 minute from now
-   *
-  /**
-   * Creates a cron(..) expression for a single instance at Now+minutesFromNow
-   * NOTE: CloudWatchEvents only support a 1minute granularity for re-invoke
-   * Anything less should be handled inside the original handler request
-   *
-   * @param minutesFromNow The number of minutes from now for building the cron expression
-   * @return A cron expression for use with CloudWatchEvents putRule(..) API
-   * @apiNote Expression is of form cron(minutes, hours, day-of-month, month, day-of-year, year) where
-   * day-of-year is not necessary when the day-of-month and month-of-year fields are supplied
-*/
+//GenerateOneTimeCronExpression a cron(..) expression for a single instance at Now+minutesFromNow
 func GenerateOneTimeCronExpression(minutesFromNow int, t time.Time) string {
-
 	a := t.Add(time.Minute * time.Duration(minutesFromNow))
 	return fmt.Sprintf("cron(%02d %02d %02d %02d ? %d)", a.Minute(), a.Hour(), a.Day(), a.Month(), a.Year())
 }
 
-// newUUID generates a random UUID according to RFC 4122.
+// NewUUID generates a random UUID according to RFC 4122.
 func NewUUID() (string, error) {
 	uuid := make([]byte, 16)
 	n, err := io.ReadFull(rand.Reader, uuid)

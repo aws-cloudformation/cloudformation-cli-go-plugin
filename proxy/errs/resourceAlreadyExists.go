8a9ab9d75a2d0efb6b6d100b9961da1197c1f878
Brian Terry <brianter@amazon.com>
2019-07-15 15:42:35 -0400

    Handler rename

--- proxy/errs/resourceAlreadyExists.go
+++ proxy/errs/resourceAlreadyExists.go
@@ -3,12 +3,11 @@ package errs
 import "fmt"
 
 type ResourceAlreadyExistsError struct {
-	err                string //error description
 	ResourceTypeName   string //resource type name
 	ResourceIdentifier string //resource identifier
 
 }
 
 func (e *ResourceAlreadyExistsError) Error() string {
-	return fmt.Sprintf("Resource of type '%s' with identifier '%s' already exists.\n%s", e.ResourceTypeName, e.ResourceIdentifier, e.err)
+	return fmt.Sprintf("Resource of type '%s' with identifier '%s' already exists.", e.ResourceTypeName, e.ResourceIdentifier)
 }
--- proxy/errs/resourceNotFound.go
+++ proxy/errs/resourceNotFound.go
@@ -3,12 +3,11 @@ package errs
 import "fmt"
 
 type ResourceNotFoundError struct {
-	Err                string //error description
 	ResourceTypeName   string //resource type name
 	ResourceIdentifier string //resource identifier
 
 }
 
 func (e *ResourceNotFoundError) Error() string {
-	return fmt.Sprintf("Resource of type '%s' with identifier '%s' was not found.\n%s", e.ResourceTypeName, e.ResourceIdentifier, e.Err)
+	return fmt.Sprintf("Resource of type '%s' with identifier '%s' was not found.", e.ResourceTypeName, e.ResourceIdentifier)
 }

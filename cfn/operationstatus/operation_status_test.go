448805fb00c1530acb85890bd0c49f34ad72205b
Simon Thulbourn <sthulb@users.noreply.github.com>
2019-09-12 17:26:49 +0100

    operationstatus pkg tests

---
+++ cfn/operationstatus/operation_status_test.go
@@ -0,0 +1,46 @@
+package operationstatus
+
+import (
+	"strings"
+	"testing"
+)
+
+// TestConvert tests the string to action conversion.
+func TestConvert(t *testing.T) {
+	t.Run("String List", func(t *testing.T) {
+		stringStatus := map[string]Status{
+			"InProgress": InProgress,
+			"Complete":   Complete,
+			"Failed":     Failed,
+		}
+
+		for k, v := range stringStatus {
+			if Convert(k) != v {
+				t.Fatalf("Invalid return value: %v != %v", k, v)
+			}
+		}
+	})
+
+	t.Run("Unknown operationstatus string", func(t *testing.T) {
+		if Convert("NotAnAction") != Unknown {
+			t.Fatal("Invalid return value")
+		}
+	})
+}
+
+// TestString tests the conversion from Status to string
+func TestString(t *testing.T) {
+	t.Run("OperationStatus Strings", func(t *testing.T) {
+		stringActions := map[Status]string{
+			InProgress: "InProgress",
+			Complete:   "Complete",
+			Failed:     "Failed",
+		}
+
+		for k, v := range stringActions {
+			if strings.ToUpper(k.String()) != strings.ToUpper(v) {
+				t.Fatalf("Invalid return value: %v != %v", k, v)
+			}
+		}
+	})
+}

1ebc03810a023181f9c1e2cac15824ccd918acbb
Brian Terry <brianter@amazon.com>
2019-10-14 23:24:03 -0400

    add generater

--- python/rpdk/go/codegen.py
+++ python/rpdk/go/codegen.py
@@ -1,20 +1,22 @@
+
 # pylint: disable=useless-super-delegation,too-many-locals
 # pylint doesn't recognize abstract methods
 import logging
 import shutil
-import os
 
 from rpdk.core.data_loaders import resource_stream
-from rpdk.core.jsonutils.flattener import JsonSchemaFlattener
+from rpdk.core.exceptions import InternalError, SysExitRecommendedError
+from rpdk.core.init import input_with_validation
+from rpdk.core.jsonutils.resolver import resolve_models
 from rpdk.core.plugin_base import LanguagePlugin
 
-from .model_resolver import GOModelResolver
-from .utils import safe_reserved
-from subprocess import call
+from .resolver import translate_type
+from .utils import safe_reserved, validate_path
+from pathlib import Path
 
 LOG = logging.getLogger(__name__)
 
-OPERATIONS = ("create", "read", "update", "delete", "list")
+OPERATIONS = ("Create", "Read", "Update", "Delete", "List")
 EXECUTABLE = "cfn-cli"
 
 
@@ -22,27 +24,38 @@ class GoLanguagePlugin(LanguagePlugin):
     MODULE_NAME = __name__
     RUNTIME = "go1.x"
     ENTRY_POINT = "handler"
+    TEST_ENTRY_POINT = "{}.HandlerWrapper::testEntrypoint"
     CODE_URI = "bin/"
 
     def __init__(self):
         self.env = self._setup_jinja_env(
             trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True
         )
+        self.env.filters["translate_type"] = translate_type
+        self.env.filters["safe_reserved"] = safe_reserved
         self.namespace = None
-        self.package_name = None
 
-    def _namespace_from_project(self, project):
-        self.namespace = tuple(
-            safe_reserved(s.title()) for s in project.type_info
-        )
-        self.package_name = ".".join(self.namespace)
+    def _prompt_for_go_path(self, project):
+        namespace = project.root
+
+        if ('github.com' in namespace.parts) :
+            projectpath = namespace.parents[namespace.parts.index('github.com')- 2]
+            namepath = namespace.relative_to(projectpath)
+            prompt = "Enter the GO Import path (empty for default '{}'): ".format(str(namespace.relative_to(projectpath)))
+
+        else :
+            prompt = "Enter the GO Import path"
+
+
+        self.import_path = input_with_validation(prompt, validate_path(namepath))
+        project.settings["importpath"] = str(self.import_path )
 
     def init(self, project):
         LOG.debug("Init started")
 
-        self._namespace_from_project(project)
-        project.runtime = self.RUNTIME
-        project.entrypoint = self.ENTRY_POINT.format(self.package_name)
+        self._prompt_for_go_path(project)
+
+        self._init_settings(project)
 
         # .gitignore
         path = project.root / ".gitignore"
@@ -93,12 +106,6 @@ class GoLanguagePlugin(LanguagePlugin):
         LOG.debug("Writing handlers and tests")
         self.init_handlers(project, src)
 
-        template = self.env.get_template("callback.go.tple")
-        LOG.debug("Writing Callback Context")
-        path = src / "{}.go".format("callbackContext")
-        contents = template.render()
-        project.safewrite(path, contents)
-
         # README
         path = project.root / "README.md"
         LOG.debug("Writing README: %s", path)
@@ -113,61 +120,45 @@ class GoLanguagePlugin(LanguagePlugin):
 
         LOG.debug("Init complete")
 
+    def _init_settings(self, project):
+        project.runtime = self.RUNTIME
+        project.entrypoint = self.ENTRY_POINT.format(self.import_path)
+        project.test_entrypoint = self.TEST_ENTRY_POINT.format(self.import_path)
+
     def init_handlers(self, project, src):
         LOG.debug("Writing stub handlers")
-        for operation in OPERATIONS:
-            if operation == "list":
-                template = self.env.get_template("StubListHandler.go.tple")
-            else:
-                template = self.env.get_template("StubHandler.go.tple")
-            path = src / "{}Handler.go".format(operation)
-            LOG.debug("%s handler: %s", operation, path)
-            contents = template.render(
-                model_name=self.namespace[2],
-                operation=operation,
-            )
-            project.safewrite(path, contents)
+        template = self.env.get_template("StubHandler.go.tple")
+        path = src / "Handlers.go"
+        contents = template.render()
+        project.safewrite(path, contents)
+
 
     def _get_generated_root(self, project):
-        self._namespace_from_project(project)
-        return project.root / "cmd"  /  "resource"
+        LOG.debug("Init started")
+
 
     def generate(self, project):
         LOG.debug("Generate started")
+        root = project.root / "cmd"
 
-        self._namespace_from_project(project)
+         # project folder structure
+        src = (root  / "resource")
 
-        objects = JsonSchemaFlattener(project.schema).flatten_schema()
-
-        # project folder structure
-        src = (project.root / "cmd"  / "resource")
-
-        model_resolver = GOModelResolver(objects, "Resource")
-        models = model_resolver.resolve_models()
-
-        LOG.debug("Writing %d models", len(models))
-
-
-        template = self.env.get_template("model.go.tple")
-        for model_name, properties in models.items():
-            path = src / "{}.go".format("generated")
-            LOG.debug("%s model: %s", model_name, path)
-            contents = template.render(
-                package_name=self.package_name,
-                model_name=self.namespace[2].capitalize(),
-                properties=properties,
+        LOG.debug("Writing Types")
+        models = resolve_models(project.schema)
+        template = self.env.get_template("types.go.tple")
+        path = src / "{}.go".format("Generated")
+        contents = template.render(
+            models=models,
             )
-            project.overwrite(path, contents)
+        project.overwrite(path, contents)
 
-        path = project.root / "cmd"  / "main.go"
-        parts = os.path.split(path)
+        path = root  / "main.go"
         LOG.debug("Writing project: %s", path)
         template = self.env.get_template("main.go.tple")
-        gopath='{}/src/'.format(os.environ['GOPATH'])
-        parts = parts[0].split(gopath)
+        importpath = Path(project.settings["importpath"])
         contents = template.render(
-            model_name=self.namespace[2],
-            path=parts[1] + '/resource'
+            path=importpath / 'cmd' / 'resource'
         )
         project.overwrite(path, contents)
 
@@ -175,4 +166,5 @@ class GoLanguagePlugin(LanguagePlugin):
         LOG.debug("Generate complete")
 
     def package(self, project):
-        pass
+        LOG.debug("Generate started")
+

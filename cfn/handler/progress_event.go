a565c720f15e15c98ff504ba5f91676fdc731eb0
Simon Thulbourn <sthulb@users.noreply.github.com>
2019-10-16 14:52:55 +0100

    replace context

--- cfn/handler/progress_event.go
+++ cfn/handler/progress_event.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/aws-cloudformation/aws-cloudformation-rpdk-go-plugin-thulsimo/cfn/operationstatus"
+import (
+	"context"
+
+	"github.com/aws-cloudformation/aws-cloudformation-rpdk-go-plugin-thulsimo/cfn/operationstatus"
+)
 
 // ProgressEvent represent the progress of CRUD handlers.
 type ProgressEvent struct {
@@ -20,12 +24,12 @@ type ProgressEvent struct {
 	// IN_PROGRESS event to allow the passing through of additional state or
 	// metadata between subsequent retries; for example to pass through a Resource
 	// identifier which can be used to continue polling for stabilization
-	CallbackContext interface{}
+	CallbackContext context.Context
 
 	// A callback will be scheduled with an initial delay of no less than the number
 	// of seconds specified in the progress event. Set this value to <= 0 to
 	// indicate no callback should be made.
-	CallbackDelaySeconds int
+	CallbackDelaySeconds int64
 
 	// The output resource instance populated by a READ/LIST for synchronous results
 	// and by CREATE/UPDATE/DELETE for final response validation/confirmation

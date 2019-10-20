ad8b743e3e1ce80639bf977f97f8509762e74f54
Brian Terry <brianter@amazon.com>
2019-10-20 12:30:01 -0400

    added gen

--- python/rpdk/go/codegen.py
+++ python/rpdk/go/codegen.py
@@ -19,6 +19,8 @@ LOG = logging.getLogger(__name__)
 OPERATIONS = ("Create", "Read", "Update", "Delete", "List")
 EXECUTABLE = "cfn-cli"
 
+class GoExecutableNotFoundError(SysExitRecommendedError):
+    pass
 
 class GoLanguagePlugin(LanguagePlugin):
     MODULE_NAME = __name__
@@ -162,9 +164,41 @@ class GoLanguagePlugin(LanguagePlugin):
         )
         project.overwrite(path, contents)
 
+    @staticmethod
+    def _find_jar(project):
+        exe_glob = list(
+            (project.root / "bin").glob(
+                "{}.exe".format('handler')
+            )
+        )
+        if not exe_glob:
+            LOG.debug("No Go executable match")
+            raise GoExecutableNotFoundError(
+                "No Go executable was found.\n"
+                "Please run 'make' or the equivalent command "
+                "in your IDE to compile and package the code."
+            )
+
+        if len(exe_glob) > 1:
+            LOG.debug(
+                "Multiple Go executable match: %s",
+                ", ".join(str(path) for path in exe_glob),
+            )
+            raise InternalError("Multiple Go executable match")
+
+        return exe_glob[0]
 
         LOG.debug("Generate complete")
+    def package(self, project, zip_file):
+        def write_with_relative_path(path):
+            relative = path.relative_to(project.root)
+            zip_file.write(path.resolve(), str(relative))
 
-    def package(self, project):
-        LOG.debug("Generate started")
+        jar = self._find_jar(project)
+        write_with_relative_path(jar)
+        #write_with_relative_path(project.root / "pom.xml")
+
+        #for path in (project.root / "src").rglob("*"):
+        #    if path.is_file():
+        #        write_with_relative_path(path)
 

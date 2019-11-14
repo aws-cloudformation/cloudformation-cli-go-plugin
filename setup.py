2ff30d8af93ab35427d8dc2007a33a543c9f6194
Simon Thulbourn <sthulb@users.noreply.github.com>
2019-11-14 14:55:47 +0000

    Make (#65)
    * change dep name to match the rpdk's new name
    
    * adds target for make build-prod

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # package_data -> use MANIFEST.in instead
     include_package_data=True,
     zip_safe=True,
-    install_requires=["aws-cloudformation-rpdk>=0.1,<0.2"],
+    install_requires=["aws-cfn-cli>=0.1,<0.2"],
     entry_points={"rpdk.v1.languages": ["golang = rpdk.go.codegen:GoLanguagePlugin"]},
     license="Apache License 2.0",
     classifiers=(

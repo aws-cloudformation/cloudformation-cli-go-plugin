e313906d737c2eb3e2f90ce1aa9e6dc85c4404fd
Brian Terry <brianter@amazon.com>
2019-12-06 23:46:14 -0500

    Add fields to Response based on app spec

--- cfn/response.go
+++ cfn/response.go
@@ -8,11 +8,32 @@ import (
 // cloudformation service from a resource handler.
 // The zero value is ready to use.
 type response struct {
-	Message         string            `json:"message,omitempty"`
-	OperationStatus handler.Status    `json:"operationStatus,omitempty"`
-	ResourceModel   interface{}       `json:"resourceModel,omitempty"`
-	ErrorCode       handler.ErrorCode `json:"errorCode,omitempty"`
-	BearerToken     string            `json:"bearerToken,omitempty"`
+	// Message which can be shown to callers to indicate the nature of a
+	//progress transition or callback delay; for example a message
+	//indicating "propagating to edge"
+	Message string `json:"message,omitempty"`
+
+	//The operationStatus indicates whether the handler has reached a terminal
+	//state or is still computing and requires more time to complete
+	OperationStatus handler.Status `json:"operationStatus,omitempty"`
+
+	//ResourceModel it The output resource instance populated by a READ/LIST for
+	//synchronous results and by CREATE/UPDATE/DELETE for final response
+	//validation/confirmation
+	ResourceModel interface{} `json:"resourceModel,omitempty"`
+
+	// ErrorCode is used to report granular failures back to CloudFormation
+	ErrorCode handler.ErrorCode `json:"errorCode,omitempty"`
+
+	// BearerToken is used to report progress back to CloudFormation and is
+	//passed back to CloudFormation
+	BearerToken string `json:"bearerToken,omitempty"`
+
+	// ResourceModels is the output resource instances populated by a LIST for synchronous results
+	ResourceModels []interface{} `json:"resourceModels,omitempty"`
+
+	// NextToken the token used to request additional pages of resources for a LIST operation
+	NextToken string `json:"nextToken,omitempty"`
 }
 
 // newFailedResponse returns a response pre-filled with the supplied error

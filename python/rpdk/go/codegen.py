0949c68494bc8de4026357e215b0bb830e154218
Omkar Hegde <omkhegde@amazon.com>
2020-09-18 09:38:03 -0700

    Don't overwrite Makefile on generate

--- python/rpdk/go/codegen.py
+++ python/rpdk/go/codegen.py
@@ -73,6 +73,13 @@ class GoLanguagePlugin(LanguagePlugin):
         inter = project.root / "internal"
         inter.mkdir(parents=True, exist_ok=True)
 
+        # Makefile
+        path = project.root / "Makefile"
+        LOG.debug("Writing Makefile: %s", path)
+        template = self.env.get_template("Makefile")
+        contents = template.render()
+        project.overwrite(path, contents)
+
         # go.mod
         path = project.root / "go.mod"
         LOG.debug("Writing go.mod: %s", path)
@@ -163,10 +170,10 @@ class GoLanguagePlugin(LanguagePlugin):
         project.overwrite(path, contents)
         format_paths.append(path)
 
-        # Makefile
-        path = project.root / "Makefile"
-        LOG.debug("Writing Makefile: %s", path)
-        template = self.env.get_template("Makefile")
+        # makebuild
+        path = project.root / "makebuild"
+        LOG.debug("Writing makebuild: %s", path)
+        template = self.env.get_template("makebuild")
         contents = template.render()
         project.overwrite(path, contents)
 

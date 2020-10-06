700550dd4ec6019aa7b050c352438eb07721a213
Brian Terry <brianter@amazon.com>
2020-10-06 15:01:30 -0400

    Fix ignored errors from Unstringify. (#160)
    * check forinvalid properties
    
    * fix typo

--- cfn/encoding/unmarshal.go
+++ cfn/encoding/unmarshal.go
@@ -7,12 +7,16 @@ import (
 // Unmarshal converts stringified-JSON into the passed-in type
 func Unmarshal(data []byte, v interface{}) error {
 	var dataMap map[string]interface{}
-	err := json.Unmarshal(data, &dataMap)
+	var err error
+	err = json.Unmarshal(data, &dataMap)
 	if err != nil {
 		return err
 	}
 
-	Unstringify(dataMap, v)
+	err = Unstringify(dataMap, v)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
